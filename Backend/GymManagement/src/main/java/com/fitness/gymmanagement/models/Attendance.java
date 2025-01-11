c00440770cd249dde176a0bf623307be2a3bbefd
Joyboy <anuj201982@gmail.com>
2025-01-11 15:29:33 +0530

    Add the properties file

--- Backend/GymManagement/src/main/java/com/fitness/gymmanagement/models/Attendance.java
+++ Backend/GymManagement/src/main/java/com/fitness/gymmanagement/models/Attendance.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 public class Attendance {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private long id;
 
