191209b95ec04cb1f082f919dae4e915e29e4def
Joyboy <anuj201982@gmail.com>
2025-01-18 15:47:06 +0530

    Add header, footer and background image

---
+++ Frontend/gym_management/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <div>
+        <a href="#">Contact Us</a>
+      </div>
+      <div>
+        <a href="#">Our Trainers</a>
+      </div>
+      <div>
+        <a href="#">Our Services</a>
+      </div>
+    </div>
+  )
+}
---
+++ Frontend/gym_management/src/components/TopNavbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function TopNavbar() {
+  return (
+    <div className='topBar'>
+      <div><a href='#'>Home</a></div>
+      <div><a href='#'>Reviews</a></div>
+      <div><a href='#'>Book a visit</a></div>
+      <div><a href='#'>Log in/Sign up</a></div>
+    </div>
+  )
+}
